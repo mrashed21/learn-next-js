21b7c9765bedeeec9298e47846a90f8341f1e6f6
mrashed21 <muhammadrashed760@gmail.com>
2025-02-14 18:48:17 +0600

    [META-DATA] learn metadata

--- src/app/blog/[blogid]/page.tsx
+++ src/app/blog/[blogid]/page.tsx
@@ -1,4 +1,16 @@
-const page = ({ params }: { params: { blogid: string } }) => {
+import { Metadata } from "next";
+type Props = {
+  params: {
+    blogid: string[];
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Blog ${params.blogid}`,
+  };
+};
+const page = ({ params }: Props) => {
   return (
     <div>
       <h1>this {params.blogid}</h1>
