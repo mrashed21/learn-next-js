2b9a046c47972ca207228ef72c01419a595ee545
mrashed21 <muhammadrashed760@gmail.com>
2025-02-14 22:36:35 +0600

    [useRouter] learn useRouter

--- src/app/blog/[blogid]/page.tsx
+++ src/app/blog/[blogid]/page.tsx
@@ -1,19 +1,25 @@
-import { Metadata } from "next";
+"use client";
+import { useRouter } from "next/navigation";
 type Props = {
   params: {
     blogid: string[];
   };
 };
 
-export const generateMetadata = ({ params }: Props): Metadata => {
-  return {
-    title: `Blog ${params.blogid}`,
-  };
-};
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Blog ${params.blogid}`,
+//   };
+// };
 const page = ({ params }: Props) => {
+  const router = useRouter();
+  const handleRoute = () => {
+    router.push("/");
+  };
   return (
     <div>
       <h1>this {params.blogid}</h1>
+      <button onClick={handleRoute}>Go Home</button>
     </div>
   );
 };
